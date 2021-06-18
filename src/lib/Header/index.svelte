5ff12809a8e1d625f4297cf811dfe12f1f2e0b37
yuki.tomita <yuki.tomita@d2cdot.co.jp>
2021-06-18 19:33:06 +0900

    create set up

---
+++ src/lib/Header/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<header class="bg-gray-800">
+  <h1>header</h1>
+</header>
+
+<style lang="scss">
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import '../styles/app.scss';
+  import Header from '$lib/Header/index.svelte';
+  import Footer from '$lib/Footer/index.svelte';
+</script>
+
+<Header />
+
+<main>
+  <slot />
+</main>
+
+<Footer />
+
+<style lang="scss">
+</style>
